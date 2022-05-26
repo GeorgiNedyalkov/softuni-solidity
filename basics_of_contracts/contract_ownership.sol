b229539db6de6f80a5ba1195582b3a863d05ae27
Flu3nt <gnedyalkov94@gmail.com>
2022-05-26 09:51:23 +0300

    Second version of the Contract

--- basics_of_contracts/contract_ownership.sol
+++ basics_of_contracts/contract_ownership.sol
@@ -1,26 +1,30 @@
 pragma solidity >=0.4.22 <0.5.5;
 
 contract ContractOwnership {
-    address owner;
-
-    event OwnershipChanged(address oldOwner, address newOwner);
+    event OwnershipChanged(address indexed oldOwner, address indexed newOwner);
     event FallbackEvent(address sender, uint256 value);
+
+    address public owner;
 
     constructor() public {
         owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(owner == msg.sender);
+        require(owner == msg.sender, "Only the owner can execute the function");
         _;
     }
 
+    modifier onlyProposedOwner() {}
+
     function changeOwner(address newOwner) public onlyOwner {
+        address oldOwner = owner;
         newOwner = owner;
-        emit OwnershipChanged(owner, newOwner);
+        emit OwnershipChanged(oldOwner, newOwner);
     }
 
     function acceptOwnership() public onlyOwner {
+        require(msg.sender == newOwner);
         uint256 timeLimit = block.timestamp + 30 seconds;
         require(block.timestamp < timeLimit);
     }
